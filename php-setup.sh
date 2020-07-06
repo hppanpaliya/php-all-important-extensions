b79dd319f47f70094ce7bbafc0cfde219f9fb6c3
hppanpaliya <44992081+hppanpaliya@users.noreply.github.com>
2020-07-06 20:15:18 +0530

    Create php-setup.sh

---
+++ php-setup.sh
@@ -0,0 +1,2 @@
+sudo apt-get update
+sudo apt-get install -y php libapache2-mod-php php-cgi php-cli php-curl php-imap php-gd php-mysql php-pgsql php-sqlite3 php-mbstring php-json php-bz2 php-xmlrpc php-gmp php-xsl php-soap php-xml php-zip php-dba
